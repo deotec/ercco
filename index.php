7b2f4f6c3a8acd0ae10d4652c476f40b0e961a63
Douglas Dias <deotecnology@gmail.com>
2016-06-19 08:54:50 -0300

    commit02
    segundo commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/** @package    ERCCO01 */
+/** @package    ERCCO010101 */
 
 /* GlobalConfig object contains all configuration information for the app */
 include_once("_global_config.php");
